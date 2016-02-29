d1e7bb3cbdb9e2585ff9d25b3ad04103b9f86e73
Allison Paul <allison.paul221@gmail.com>
2016-02-29 14:50:29 -0600

    Add address directory and 4.3.1

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,12 @@
+
+# Format an Address
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def make_address (street, city, state, zip)
+   puts "You live at #{street}, in the beautiful city of #{city}, #{state}. Youre zip code is #{zip.to_s}."
+ end
+  make_address("sumpter avenue", "bowling green", "ky", "42101")
