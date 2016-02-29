28f70a43025ec4692a396a97d945b9b870a816a4
Allison Paul <allison.paul221@gmail.com>
2016-02-29 14:27:21 -0600

    Add Creating Methods Release 4.3

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,27 @@
+#Try it Out
+
+puts "Hi! What's your first name?"
+first_name=gets.chomp
+puts "Can I get your middle name?"
+middle_name= gets.chomp
+puts "And your last name?"
+last_name=gets.chomp
+puts "Nice to meet you, #{first_name} #{middle_name} #{last_name}."
+
+
+
+puts "Whats your very favorite number?"
+num= gets.chomp.to_i
+puts "But #{num+1} is bigger and better! Could that be a new favorite?"
+
+
+
+=begin
+How do you define a local variable?
+How do you define a method?
+What is the difference between a local variable and a method?
+How do you run a ruby program from the command line?
+    I have been running a ruby program by typing ruby and the file name.  You can also run a ruby by using IRB.This is useful when running commands individually.
+How do you run an RSpec file from the command line?
+What was confusing about this material? What made sense?
+=end
