21fb3f78bfcf8fac427697ce8966c1f4c06f1781
Allison Paul <allison.paul221@gmail.com>
2016-03-02 19:11:07 -0600

    Add to 4.5

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,17 @@
+# Simple Substrings
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def welcome(address)
+  state= address.split
+
+for word in state
+  if word == "CA" || word== "California"
+    return "Welcome to California"
+  end
+end
+ return "You should move to California"
+end
