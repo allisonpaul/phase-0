91e0051956ae31ba3fef5d3c55ea948d028bc312
Allison Paul <allison.paul221@gmail.com>
2016-03-04 11:05:12 -0600

    Add case method solution

--- week-4/simple-substrings/my_solution.rb
+++ week-4/simple-substrings/my_solution.rb
@@ -1,9 +1,31 @@
 # Simple Substrings
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # Your Solution Below
+#intial solution using a case method.
+
+def welcome(street, city, state)
+  case state
+
+   when "California"
+    return "Welcome to California!"
+    when "CA"
+    return "Welcome to California!"
+    else
+    return "You should move to California"
+    end
+  end
+
+  welcome("1833 Sawyer", "Chicago", "California")
+
+
+
+
+
+=begin
+refactored solution using split method
 
 def welcome(address)
   state= address.split
@@ -15,3 +37,5 @@ for word in state
 end
  return "You should move to California"
 end
+
+=end
