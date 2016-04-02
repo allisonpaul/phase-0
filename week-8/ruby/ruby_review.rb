7fd528ba875907177cb0c0171390ecb03d450d29
Allison Paul <allison.paul221@gmail.com>
2016-04-02 11:45:16 -0500

    Add Ruby review

---
+++ week-8/ruby/ruby_review.rb
@@ -0,0 +1,37 @@
+# Reverse Words
+
+
+# I worked on this challenge [by myself, with: ].
+# This challenge took me [#] hours.
+
+# Pseudocode
+# input-a string
+# output-the string reversed
+#STEPS
+#Define a method reverse_words that takes a string argument
+#-Split the string into an array
+# -Go through each word of the array
+# -reverse each word
+#-push into a new array
+#-join the new array back into a string.
+# -return the new string.
+
+
+# Initial Solution
+
+def reverse_words(string)
+  words=string.split(" ")
+  array=[]
+  words.each do |word|
+    array.push(word.reverse!)
+  end
+  p array.join(" ")
+end
+
+
+reverse_words('this is going to work')
+
+
+
+
+# Reflection
